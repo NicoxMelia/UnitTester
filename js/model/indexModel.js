972466a0b3f8bb06889cce3a665dfaa4abe8aba4
nicox <nicolas.melia@mi.unc.edu.ar>
2025-07-05 20:34:05 -0300

    path changed

--- js/model/indexModel.js
+++ js/model/indexModel.js
@@ -1,6 +1,7 @@
 export class IndexModel{
     async getExercises() {
-        const response = await fetch('../../data/excercices.json');
+       // const response = await fetch('../../data/excercices.json');
+        const response = await fetch('data/excercises.json');
         if (!response.ok) throw new Error(`HTTP error: ${response.status}`);
         return await response.json();
     }
